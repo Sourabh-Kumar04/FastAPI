aba398f393297707ec7c1e3d55241e380be7f144
Sourabh-Kumar04 <sourabhkumar01010101@gmail.com>
2025-08-01 02:41:21 +0000

    ✨ Implemet post request in fastAPI using create function

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field,  EmailStr
+from typing import Annotated, Literal,  List, Optional
 import json
+from datetime import date
 
 app = FastAPI()
 
+from pydantic import BaseModel, Field, EmailStr
+from typing import Annotated, Literal, List, Optional
+from datetime import date
+
+class Patient(BaseModel):
+
+    id: Annotated[int, Field(..., ge=1, description="Unique ID of the patient", example=1)]
+    name: Annotated[str, Field(..., min_length=1, description="Full name of the patient")]
+    age: Annotated[int, Field(..., gt=0, lt=130, description="Age of the patient")]
+    gender: Annotated[Literal['Male', 'Female', 'Other'], Field(..., description="Gender of the patient")]
+    contact: Annotated[str, Field(..., min_length=10, max_length=15, description="Contact number")]
+    email: Annotated[EmailStr, Field(..., description="Email address of the patient")]
+    address: Annotated[str, Field(..., description="Residential address")]
+    blood_group: Annotated[str, Field(..., pattern=r"^(A|B|AB|O)[+-]$", description="Blood group like A+, O-, AB+ etc.")]
+    medical_history: Annotated[List[str], Field(..., description="List of past medical issues")]
+    admission_date: Annotated[date, Field(..., description="Date of hospital admission")]
+    discharge_date: Annotated[Optional[date], Field(None, description="Date of discharge (if any)")]
+    doctor_assigned: Annotated[str, Field(..., description="Name of the doctor assigned")]
+    current_status: Annotated[Literal['Admitted', 'Discharged', 'Under Observation'], Field(..., description="Current patient status")]
+
 def load_data():
-    with open ('patient_dataset.json', 'r') as f:
+    with open('patient_dataset.json', 'r') as f:
         data = json.load(f)
 
     return data
+
+def save_data(data):
+    with open('patient_dataset.json', 'w') as f:
+        json.dump(data, f)
 
 @app.get('/')
 def home():
@@ -24,7 +52,7 @@ def view():
     return data
 
 @app.get('/patient/{id}')  # add dynamic segement using {}
-def view_patient(id: int = Path(..., description='ID of the patient in the DB', example='1')):
+def view_patient(id: int = Path(..., description='ID of the patient in the DB', example=1)):
     # load all the data
     data = load_data()
     
@@ -53,3 +81,21 @@ def sort_patient(sort_by: str = Query(..., description="Sort on the basis of age
     sorted_data = sorted(data, key=lambda x: x.get(sort_by, 0), reverse=sort_order)
 
     return sorted_data
+
+@app.post("/create")
+def create_patient(patient: Patient):
+
+    # load data
+    data = load_data()
+
+    # check if alredy exist
+    for existing in data:
+        if existing['id'] == patient.id:
+            raise HTTPException(status_code=400, detail='Patient already exists')
+
+    data.append(patient.model_dump())  
+
+    # save into the json file
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message': 'Patient created successfully'})
